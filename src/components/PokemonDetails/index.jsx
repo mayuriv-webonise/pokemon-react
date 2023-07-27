a7136ecfa0261a8698cd8431576ac5c9844268e6
mayuriv-webonise <mayuri.vibhute@webonise.com>
2023-07-27 17:26:54 +0530

    undefined change

--- src/components/PokemonDetails/index.jsx
+++ src/components/PokemonDetails/index.jsx
@@ -25,9 +25,9 @@ function PokemonDetails() {
       <Card sx={{ boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,1.12)', width: '100%', height: '60vh', padding: '30px' }}>
         <CardMedia
           component="img"
-          alt={details.name}
+          alt={details?.name}
           height="70%"
-          image={details.sprites.front_default} />
+          image={details?.sprites?.front_default} />
         <CardContent>
           <Typography variant="h5" gutterBottom>
             ID - {details.id}
