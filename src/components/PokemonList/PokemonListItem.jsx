4c1baec98462f3fea257ae4451c8777aacce89ab
mayuriv-webonise <mayuri.vibhute@webonise.com>
2023-07-28 10:43:02 +0530

    spaces changes

--- src/components/PokemonList/PokemonListItem.jsx
+++ src/components/PokemonList/PokemonListItem.jsx
@@ -9,6 +9,7 @@ const { updateData } = React.useContext(DetailContext);
   const handleNameClick = () => {
     updateData({url: url});
   }
+  
   return (
 
   <Card sx={{margin:'10px', boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,1.12)'}}>
